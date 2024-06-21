523ff0728a9c9ce22a2f731cf4c3d0ae02372735
Maxnightik <xiaomimax4n@gmail.com>
2024-06-21 15:54:22 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-import { API_URL } from "./config.js";
-import { price } from "./config.js";
+const API_URL = "https://flash-sponge-orca.glitch.me/";
+
+const price = {
+  Полуниця: 60,
+  Банан: 50,
+  Манго: 70,
+  Ківі: 55,
+  Маракуйя: 90,
+  Яблуко: 45,
+  "М'ята": 50,
+  Лід: 10,
+  Біорозкладний: 20,
+  Пластиковий: 0,
+};
 
 const cartDataControl = {
   get() {
